f6d007cc30efcecc4b41b6aa6b3a500033847910
DIVINEakisa <akisadivine11@gmail.com>
2025-10-14 16:07:59 +0200

    Object Exercises

---
+++ Object/obj.js
@@ -0,0 +1,14 @@
+"use strict";
+let fullName = "Oluwatobi Sofela";
+function profile() {
+  const fullName = "Tobi Sho";
+  function sayName() {
+    const fullName = "Oluwa Sofe";
+    function writeName() {
+      return fullName;
+    }
+    return writeName();
+  }
+  return sayName();
+}
+console.log(profile());
---
+++ Object/reserved.js
@@ -0,0 +1,6 @@
+let obj = {
+  class: 1,
+  if: 3,
+  return: 7,
+};
+console.log(obj.class);
